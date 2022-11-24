96f62039cc1322d74eea554d4821f90270fe16cc
Juan Jimenez <juanjiimenez8@gmail.com>
2022-11-24 17:52:20 -0500

    integrantes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,6 +6,8 @@ function Home() {
         <h1>Desarrollo Web 1</h1>
         <h4>Proyecto de login Desarrollo web</h4>
         <h5>Juan Jimenez</h5>
+        <h5>Benjamin Nieto</h5>
+        <h5>Roiner Escobar</h5>
     </div>
     </>);
 }
